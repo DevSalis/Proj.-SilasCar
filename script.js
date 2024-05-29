398b71bf640e31d4a556233e0b40128b9756799f
MrSalis <philip.cost@hotmail.com>
2024-05-29 10:43:38 -0300

    Novos dados na array, e function lincado aos buttons

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 const menuOptions = [
 
-    { name: 'X-Salada', price: 30, vegan: false, src: './img/xsalada.jpeg' },
-    
-    { name: 'X-Bacon', price: 34, vegan: false, src: './img/xbacon.png' },
-    
-    { name: 'X-Bacon Egg', price: 39, vegan: false, src: './img/bacon-egg.png' },
-    
-    { name: 'Monstruoso', price: 50, vegan: false, src: './img/monstruoso.png' },
-    
-    { name: 'Big Vegano', price: 55, vegan: true, src: './img/xvegan.png' },
-    
-    { name: 'X-Vegan', price: 45, vegan: true, src: './img/monstruoso-vegan.png' },
-    
-    ]
+    { name: 'Mercedes', price: 540, popular: false, src: './assets/car 1.png' },
+
+    { name: 'Gol', price: 75, popular: true, src: './assets/car 10.png' },
+
+    { name: 'Cruze', price: 148, popular: false, src: './assets/car 11.png' },
+
+    { name: 'Mercedes sports', price: 970, popular: false, src: './assets/car 6.png' },
+
+    { name: 'Audi', price: 680, popular: false, src: './assets/car 2.png' },
+
+    { name: 'Honda', price: 140, popular: false, src: './assets/car 3.png' },
+
+    { name: 'BMW', price: 470, popular: false, src: './assets/car 4.png' },
+
+    { name: 'Camaro', price: 480, popular: false, src: './assets/car 5.png' },
+
+    { name: 'Ferrari', price: 3.400, popular: false, src: './assets/car 7.png' },
+
+    { name: 'Porsche', price: 630, popular: false, src: './assets/car 8.png' },
+
+    { name: 'Celta', price: 25, popular: true, src: './assets/car 9.png' },
+
+    { name: 'Kwid', price: 52, popular: true, src: './assets/car 12.png' },
+
+]
+
+const myButtons = document.querySelector("button")
+
+function clickOnButton() {
+
+   console.log("Esta funcionando")
+} 
