4b292f96ab21f0a8cde71d5268f023657fae9480
MrSalis <philip.cost@hotmail.com>
2024-05-30 02:32:35 -0300

    Iniciando js - desafio foreach

--- script.js
+++ script.js
@@ -1,34 +1,7 @@
-const menuOptions = [
 
-    { name: 'Mercedes', price: 540, popular: false, src: './assets/car 1.png' },
 
-    { name: 'Gol', price: 75, popular: true, src: './assets/car 10.png' },
+const clickOnButton = document.querySelector("button")
 
-    { name: 'Cruze', price: 148, popular: false, src: './assets/car 11.png' },
-
-    { name: 'Mercedes sports', price: 970, popular: false, src: './assets/car 6.png' },
-
-    { name: 'Audi', price: 680, popular: false, src: './assets/car 2.png' },
-
-    { name: 'Honda', price: 140, popular: false, src: './assets/car 3.png' },
-
-    { name: 'BMW', price: 470, popular: false, src: './assets/car 4.png' },
-
-    { name: 'Camaro', price: 480, popular: false, src: './assets/car 5.png' },
-
-    { name: 'Ferrari', price: 3.400, popular: false, src: './assets/car 7.png' },
-
-    { name: 'Porsche', price: 630, popular: false, src: './assets/car 8.png' },
-
-    { name: 'Celta', price: 25, popular: true, src: './assets/car 9.png' },
-
-    { name: 'Kwid', price: 52, popular: true, src: './assets/car 12.png' },
-
-]
-
-const myButtons = document.querySelector("button")
-
-function clickOnButton() {
-
-   console.log("Esta funcionando")
-} 
+menuOptions.forEach(element => {
+    clickOnButton = console.log(element)
+});
