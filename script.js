ea001c12ac6c1ad61ea7620c5dde0302d2a90f2d
MrSalis <philip.cost@hotmail.com>
2024-06-04 00:51:09 -0300

    refazer desafio

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
 
-const myUl = document.querySelector("ul")
-let myLi = ""
-
-function showAll() {
-    menuOptions.forEach(element => {
-
-        myLi += `
-        <li>
-        <img class="cars" src="${element.src}">
-            <p>${element.name}</p>
-            <p class="valor">R$ ${element.price}</p>
-    </li>`
-       
-        
-       
-    });
-
-    myUl.innerHTML = myLi
-
-}
-
-const myButton = document.querySelector("#buttonShowAll")
-
-myButton.addEventListener("click", showAll)
 
 
