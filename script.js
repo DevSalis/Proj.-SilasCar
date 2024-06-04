c838479146d755b809b4894d0d0912ca860de58f
MrSalis <philip.cost@hotmail.com>
2024-06-04 03:31:19 -0300

    Iniciando desafio desconto com map

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 
 
 
+const minhaUl = document.querySelector('ul')
+const meuBotao = document.querySelector('#buttonShowAll')
+const desconto = document.querySelector('#buttonOf')
+let minhaLi = ''
+
+function mostrarTudo() {
+    menuOptions.forEach(element => {
+        minhaLi += `
+           <li>
+               <img class="cars" src="${element.src}">
+                <p>${element.name}</p>
+                <p class="valor">R$ ${element.price}</p>
+           </li>`
+
+           minhaUl.innerHTML = minhaLi
+    });
+}
+function catalogoOf(){
+
+    console.log("tem desconto?")
+}
+
+
+meuBotao.addEventListener('click', mostrarTudo)
+desconto.addEventListener('click', catalogoOf)
+
+
