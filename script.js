ced9a9f03c11827690de69f1ff7c95424a857ad1
MrSalis <philip.cost@hotmail.com>
2024-06-23 01:04:31 -0300

    reiniciando a partir do fim dos desafios

--- script.js
+++ script.js
@@ -1,72 +1,71 @@
-const minhaUl = document.querySelector('ul');
-const meuCatalogo = document.querySelector('#buttonShowAll');
-const meuDesconto = document.querySelector('#buttonOf');
-const minhaSoma = document.querySelector('#buttonTotal');
-const maisBarato = document.querySelector('#button-populares');
-const nav = document.querySelector('.box-buttons');
+
+const minhaUl = document.querySelector('ul')
+const meuCatalogo = document.querySelector('#buttonShowAll')
+const meuDesconto = document.querySelector('#buttonOf')
+const minhaSoma = document.querySelector('#buttonTotal')
+const maisBarato = document.querySelector('#button-populares')
 
 
 function mostrarTudo(newarray) {
-    let minhaLi = '';
+
+    let minhaLi = ''
 
     newarray.forEach(element => {
         minhaLi += `
-                    <li>
-                        <img class="cars" src="${element.src}">
-                        <p>${element.name}</p>
-                        <p class="valor">R$ ${element.price}</p>
-                    </li>`;
+           <li>
+               <img class="cars" src="${element.src}">
+                <p>${element.name}</p>
+                <p class="valor">R$ ${element.price}</p>
+           </li>`
 
-        minhaUl.innerHTML = minhaLi;
+        minhaUl.innerHTML = minhaLi
     });
 }
 
-function blackFriday() {
+function blackFriday(){
     const of10 = menuOptions.map((of) => ({
-        ...of,
-        price: of.price * 0.9,
-    }));
-    mostrarTudo(of10);
+       ...of,
+       price: of.price * 0.9, 
+    }))
+
+    mostrarTudo(of10)
 }
 
 function total() {
-    const buyAll = menuOptions.reduce((acc, soma) => {
-        return acc + soma.price;
-    }, 0);
+     
+
+    const buyAll =  menuOptions.reduce((acc, soma) =>{
+        return acc + soma.price
+    },0)
 
     const of10 = menuOptions.map((of) => ({
         ...of,
-        price: of.price * 0.9,
-    }));
+        price: of.price * 0.9, 
+     }))
 
-    const ofTotal = of10.reduce((acc, curr) => acc + curr.price, 0);
+     const ofTotal = of10.reduce((acc, curr) => acc + curr.price, 0)
+
 
     minhaUl.innerHTML = `
-                <li class="valor-total">
-                    <p class="valor-total-1">Total  ${buyAll}</p>
-                    <p class="valor-total-1">Total com desconto ${ofTotal}</p>
-                </li>`;
+    <li class="valor-total">
+         <p class="valor-total-1">Total  ${buyAll}</p>
+         <p class="valor-total-1">Total com desconto ${ofTotal}</p>
+    </li>`
 
-    console.log(buyAll, ofTotal);
+    console.log(buyAll, ofTotal)
+   
 }
 
 function barato() {
-    const popularCar = menuOptions.filter((maisBarato) => maisBarato.popular);
-    mostrarTudo(popularCar);
+    
+const popularCar = menuOptions.filter((maisBarato) => maisBarato.popular)
+mostrarTudo(popularCar)
 }
 
-meuCatalogo.addEventListener('click', function () {
 
-    mostrarTudo(menuOptions);
-    nav.style.display = 'flex';
-});
 
-meuDesconto.addEventListener('click', blackFriday);
-minhaSoma.addEventListener('click', total);
-maisBarato.addEventListener('click', barato);
+meuCatalogo.addEventListener('click', ()=> mostrarTudo(menuOptions))
+meuDesconto.addEventListener('click', blackFriday)
+minhaSoma.addEventListener('click', total)
+maisBarato.addEventListener('click', barato)
 
-document.querySelector('a[href=".top"]').addEventListener('click', function (event) {
-    event.preventDefault();
-    window.scrollTo({ top: 0, });
-    document.querySelector('.box-buttons').style.display = 'none';
-});
