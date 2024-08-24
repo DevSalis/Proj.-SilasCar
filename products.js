7ddd6e03e2ff7060320890c7db16a4579b4b00b4
MrSalis <philip.cost@hotmail.com>
2024-08-24 01:28:10 -0300

    novo gol gti e atualização de preço e ajuste na img do kwid

--- products.js
+++ products.js
@@ -1,6 +1,6 @@
 const menuOptions = [
 
-    { name: 'Gol', price: 70, popular: true, src: './assets/car 10.png' },
+    { name: 'Gol GTi', price: 100, popular: true, src: './assets/car 10.png' },
 
     { name: 'Cruze', price: 150, popular: false, src: './assets/car 11.png' },
 
