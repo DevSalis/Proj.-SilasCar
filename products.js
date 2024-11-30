0f3bfb1352bf4bf180841a316864e72b565779cb
MrSalis <philip.cost@hotmail.com>
2024-11-29 23:22:01 -0300

    troca de formatos de img e video e cores do site

--- products.js
+++ products.js
@@ -1,24 +1,24 @@
 const menuOptions = [
 
-    { name: 'Gol GTi', price: 90000, popular: true, src: './assets/car 10.png' },
+    { name: 'Gol GTi', price: 90000, popular: true, src: './assets/car 10.webp' },
 
-    { name: 'Cruze', price: 150000, popular: false, src: './assets/car 11.png' },
+    { name: 'Cruze', price: 150000, popular: false, src: './assets/car 11.webp' },
 
-    { name: 'Mercedes sports', price: 970000, popular: false, src: './assets/car 6.png' },
+    { name: 'Mercedes sports', price: 970000, popular: false, src: './assets/car 6.webp' },
 
-    { name: 'Audi', price: 680000, popular: false, src: './assets/car 2.png' },
+    { name: 'Audi', price: 680000, popular: false, src: './assets/car 2.webp' },
 
-    { name: 'BMW', price: 470000, popular: false, src: './assets/car 4.png' },
+    { name: 'BMW', price: 470000, popular: false, src: './assets/car 4.webp' },
 
-    { name: 'Camaro', price: 480000, popular: false, src: './assets/car 5.png' },
+    { name: 'Camaro', price: 480000, popular: false, src: './assets/car 5.webp' },
 
-    { name: 'Ferrari', price: 3400000, popular: false, src: './assets/car 7.png' },
+    { name: 'Ferrari', price: 3400000, popular: false, src: './assets/car 7.webp' },
 
-    { name: 'Porsche', price: 630000, popular: false, src: './assets/car 8.png' },
+    { name: 'Porsche', price: 630000, popular: false, src: './assets/car 8.webp' },
 
-    { name: 'Celta', price: 20000, popular: true, src: './assets/car 9.png' },
+    { name: 'Celta', price: 20000, popular: true, src: './assets/car 9.webp' },
 
-    { name: 'Uno', price: 20000, popular: true, src: './assets/car 12.png' },
+    { name: 'Uno', price: 20000, popular: true, src: './assets/car 12.webp' },
 
 ]
 
